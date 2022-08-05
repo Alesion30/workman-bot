981d3ed7fbcdca328ed1e3a92c126d4cf1253c80
Yuta Ohira <alesion90@gmail.com>
2022-08-05 20:53:10 +0900

    mv env

---
+++ env.ts
@@ -0,0 +1,3 @@
+export const SLACK_SIGNING_SECRET = Deno.env.get('SLACK_SIGNING_SECRET');
+export const SLACK_BOT_TOKEN = Deno.env.get('SLACK_BOT_TOKEN');
+export const SLACK_APP_TOKEN = Deno.env.get('SLACK_APP_TOKEN');
--- main.ts
+++ main.ts
@@ -1,11 +1,16 @@
 import 'https://deno.land/x/dotenv@v2.0.0/load.ts';
 import { App } from 'https://deno.land/x/slack_bolt@1.0.0/mod.ts';
 import { datetime } from 'https://deno.land/x/ptera@v1.0.2/mod.ts';
+import {
+  SLACK_APP_TOKEN,
+  SLACK_BOT_TOKEN,
+  SLACK_SIGNING_SECRET,
+} from './env.ts';
 
 const app = new App({
-  signingSecret: Deno.env.get('SLACK_SIGNING_SECRET'),
-  token: Deno.env.get('SLACK_BOT_TOKEN'),
-  appToken: Deno.env.get('SLACK_APP_TOKEN'),
+  signingSecret: SLACK_SIGNING_SECRET,
+  token: SLACK_BOT_TOKEN,
+  appToken: SLACK_APP_TOKEN,
   socketMode: true,
   ignoreSelf: true,
 });
