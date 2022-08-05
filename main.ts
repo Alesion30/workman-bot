510ed08e0b159366b6956f9f6a26066441a9e6dd
Yuta Ohira <alesion90@gmail.com>
2022-08-05 20:04:25 +0900

    add syussya flow

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import 'https://deno.land/x/dotenv@v2.0.0/load.ts';
 import { App } from 'https://deno.land/x/slack_bolt@1.0.0/mod.ts';
+import { datetime } from 'https://deno.land/x/ptera@v1.0.2/mod.ts';
 
 const app = new App({
   signingSecret: Deno.env.get('SLACK_SIGNING_SECRET'),
@@ -13,6 +14,17 @@ app.event('message', async ({ event, say }) => {
   console.log(event);
   await say('pong');
 });
+
+// 出社
+app.message(
+  RegExp(/^(:ronri_syussya:|:buturi_syussya:).*/),
+  async ({ event, say }) => {
+    console.log(event);
+    const now = datetime().toISO();
+    // deno-lint-ignore no-explicit-any
+    await say({ text: `打刻しました！${now}`, thread_ts: event.ts } as any);
+  },
+);
 
 await app.start({ port: 3000 });
 console.log('🦕 ⚡️');
